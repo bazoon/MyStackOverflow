3d26af2451f19b197207ff7bc17f15a31f0d36e8
bazoon <vith77@gmail.com>
2014-09-12 14:45:06 +0600

    added main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'home/index'
+
   resources :questions do
 
   end
-
+  
+  get "/" => "home#index", :as => "root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
