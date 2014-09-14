8b4d383f1e28da9ad0944f634f7383a1763a0d60
bazoon <vith77@gmail.com>
2014-09-14 12:28:12 +0600

    added user_id to question model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140912114050) do
+
+ActiveRecord::Schema.define(version: 20140914062058) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140912114050) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
