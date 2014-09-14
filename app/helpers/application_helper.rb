afc4fa5d7997456f846bf9ba8fb3da5dde2475ca
bazoon <vith77@gmail.com>
2014-09-14 17:23:02 +0600

    addes some helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+
+  def ld(d)
+    I18n.l(d, format: :long) unless d.nil?
+  end  
+
+  def show_icon
+    content_tag(:span,"",class: "glyphicon glyphicon-folder-open")
+  end
+
+  def edit_icon
+    content_tag(:span,"",class: "glyphicon glyphicon-edit")
+  end
+
+  def del_icon
+    content_tag(:span,"",class: "glyphicon glyphicon-trash")
+  end
+
+  def red_label(s)
+    content_tag(:span,s,class: "label label-danger")
+  end
+
+  def green_label(s)
+    content_tag(:span,s,class: "label label-success")
+  end
+
+
+  def users_icon
+    content_tag(:span,"",class: "glyphicon glyphicon-user")
+  end
+
+  def now
+    Time.zone.now
+  end
+
+
 end
