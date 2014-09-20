48ea20691232a976910fd3c2a655be5126c32200
bazoon <vith77@gmail.com>
2014-09-20 13:45:15 +0600

    change link language to en

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, except: [:index, :new, :create]
 
   def index
-    @questions = Question.all
+    @questions = Question.all.order('created_at desc')
   end
 
   def show
