1c910d235cfd3e500afce2083b240fd1ac2c7e38
bazoon <vith77@gmail.com>
2014-10-01 16:22:16 +0600

    add specs for models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe User, :type => :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :password }
   
-
+  it { should have_many(:questions) }
+  it { should have_many(:answers) }
+  it { should have_many(:comments) }
 end
