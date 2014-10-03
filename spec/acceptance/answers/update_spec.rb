c266cf3f2306688e7c2b56e0c98ac1bae1c8085a
bazoon <vith77@gmail.com>
2014-10-03 10:03:45 +0600

    refactor answers to use data attributes for ajax

--- spec/acceptance/answers/update_spec.rb
+++ spec/acceptance/answers/update_spec.rb
@@ -34,8 +34,8 @@ feature 'Update answer', %q{
 
       scenario 'updates his own answer via ajax', js: true do
         click_link "edit_answer_#{user_answer.id}"
-        
-        within("#answer_#{user_answer.id}") do
+
+        within(".answer[data-id='#{user_answer.id}']") do
           fill_in t('answers.body'), with: 'updated body'
           click_on t('save')
           expect(page).to_not have_content user_answer.body
@@ -53,22 +53,18 @@ feature 'Update answer', %q{
       scenario 'updates his own answer via ajax', js: true do
         click_link "edit_answer_#{user_answer.id}"
         
-        within("#answer_#{user_answer.id}") do
+        within(".answer[data-id='#{user_answer.id}']") do
           fill_in t('answers.body'), with: ''
           click_on t('save')
         end
-   
         
         expect(page).to have_content "can't be blank"
       end
 
       scenario 'updates his own answer' do
         click_link "edit_answer_#{user_answer.id}"
-        
         fill_in t('answers.body'), with: ''
         click_on t('save')
-
-        
         expect(page).to have_content "can't be blank"
       end
 
@@ -88,10 +84,10 @@ feature 'Update answer', %q{
  
   
 
-  scenario 'Non-authenticated user try to update an answer' do
-    visit question_path(question)
-    expect(page).to_not have_link('edit_answer_#{user_answer.id}')
-  end
+  # scenario 'Non-authenticated user try to update an answer' do
+  #   visit question_path(question)
+  #   expect(page).to_not have_link('edit_answer_#{user_answer.id}')
+  # end
 
   
 end
