ec015042fb36e01344c83d8f65b8080601e123b2
bazoon <vith77@gmail.com>
2014-10-03 08:51:50 +0600

    Refactor question ajax action to use data attributes

--- app/views/comments/create.js
+++ app/views/comments/create.js
@@ -1,3 +1,10 @@
-var new_comment_form = "#new_comment_form_<%= @commentable.class.to_s.underscore%>_<%=@commentable.id %>"
-$('#<%=@commentable.class.to_s.underscore%>_<%=@commentable.id%> .comments').html('<%= j render partial: 'comments/comments', locals: { commentable: @commentable } %>')
+var commentable_class = "<%= @commentable.class.to_s.underscore %>"
+var commentable_id = "<%= @commentable.id %>"
+var commentable_id_selector = "[data-id='"+commentable_id+"']"
+var commentable_class_selector = '.' + commentable_class
+var new_comment_form = ".new_comment_form[data-id='"+commentable_id+ "'][data-object='"+commentable_class+"']"
+
+// alert(commentable_class_selector+commentable_id_selector + ' .comments')
+
+$(commentable_class_selector+commentable_id_selector + ' .comments').html('<%= j render partial: 'comments/comments', locals: { commentable: @commentable } %>')
 $(new_comment_form).hide()
--- app/views/comments/update.js
+++ app/views/comments/update.js
@@ -1,2 +1,10 @@
-$('#<%=@comment.commentable.class_underscore%>_<%=@comment.commentable.id%> .comments').html('<%= j render partial: 'comments/comments', locals: { commentable: @comment.commentable } %>')
-$('comment_update_form_<%= @comment.commentable.id%>').hide()
+var commentable_class = "<%= @comment.commentable.class.to_s.underscore %>"
+var commentable_id = "<%= @comment.commentable.id %>"
+var commentable_id_selector = "[data-id='"+commentable_id+"']"
+var commentable_class_selector = '.' + commentable_class
+
+$(commentable_class_selector+commentable_id_selector + ' .comments').html('<%= j render partial: 'comments/comments', locals: { commentable: @comment.commentable } %>')
+
+
+// $('#<%=@comment.commentable.class_underscore%>_<%=@comment.commentable.id%> .comments').html('<%= j render partial: 'comments/comments', locals: { commentable: @comment.commentable } %>')
+// $('comment_update_form_<%= @comment.commentable.id%>').hide()
