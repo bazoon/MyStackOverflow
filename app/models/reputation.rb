37872d168434bbce340001c464307a31606101a2
bazoon <vith77@gmail.com>
2014-10-23 15:18:27 +0600

    refactor specs

--- app/models/reputation.rb
+++ app/models/reputation.rb
@@ -1,2 +1,5 @@
 class Reputation < ActiveRecord::Base
+  
+  validates :reputationable_type, :reputationable_id, presence: true
+
 end
--- spec/models/reputation_spec.rb
+++ spec/models/reputation_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Reputation, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of :reputationable_type }
+  it { should validate_presence_of :reputationable_id }
+
 end
