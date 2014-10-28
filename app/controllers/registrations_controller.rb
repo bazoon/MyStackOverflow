4aa30f105e04c64ccdf2d20db21530575fecf8a3
bazoon <vith77@gmail.com>
2014-10-28 09:15:14 +0500

    commented strong params

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,13 +3,13 @@ class RegistrationsController < Devise::RegistrationsController
  
   private
  
-  def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  # def sign_up_params
+  #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  # end
  
-  def account_update_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  # def account_update_params
+  #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  # end
   
 
   #можно через ApplicationController
