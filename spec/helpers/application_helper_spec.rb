ebfc1f1c3af018b6448eeaddc03096c6bc4440e2
bazoon <vith77@gmail.com>
2014-10-28 16:36:11 +0500

    spec for helpers

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper do
+  
+  let(:d) { Time.now } 
+
+  describe '#ld' do
+  
+    it 'converts non empty date' do
+      expect(ld(d)).to eq(I18n.l(d, format: :long))
+    end
+
+    it 'returns nil for empty date' do
+      expect(ld(nil)).to be_nil
+    end
+
+  end
+
+  describe 'icons' do
+    
+    it 'returns span with specific class' do
+      expect(show_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-folder-open")
+      expect(edit_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-edit")
+      expect(del_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-trash", alt: "delete")
+      expect(users_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-user")
+      expect(select_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-star")
+      expect(vote_up_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-thumbs-up")
+      expect(vote_down_icon).to eq content_tag(:span,"",class: "glyphicon glyphicon-thumbs-down")
+    end
+  end
+
+  describe 'labels' do
+    
+    it 'returns span with specific class' do
+      expect(red_label('foo')).to eq(content_tag(:span, 'foo', class: "label label-danger")) 
+      expect(green_label('foo')).to eq(content_tag(:span, 'foo', class: "label label-success"))   
+    end
+    
+  end
+
+
+  describe 'now' do
+    it 'returns now time' do
+      allow(Time.zone).to receive(:now) { d }
+      expect(now).to eq(d)
+    end
+  end
+
+
+
+
+end
