10d0f3650aec55cc924531f239b636730e5c5754
bazoon <vith77@gmail.com>
2014-10-28 16:55:12 +0500

    Tags_controller +1 spec

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -6,10 +6,11 @@ RSpec.describe TagsController, type: :controller do
   let!(:question) { create(:question, body: 'BODY', user_id: user.id) }
   let!(:question2) { create(:question, body: 'BODY', user_id: user.id) }
   let!(:tag) { create(:tag, name: 'TAG') }
+  let!(:tag2) { create(:tag, name: 'TAG2') }
   let!(:tagging) { create(:tagging, tag_id: tag.id, taggable_id: question.id, taggable_type: 'Question') }
   let!(:tagging2) { create(:tagging, tag_id: tag.id, taggable_id: question2.id, taggable_type: 'Question') }
 
-  describe 'GET search' do
+  describe '#GET search' do
     
     sign_in_user  
 
@@ -23,6 +24,19 @@ RSpec.describe TagsController, type: :controller do
       expect(response).to render_template 'search'
     end
 
+  end
+
+
+  describe '#GET tags' do
+
+    sign_in_user  
+
+    it 'returns searched tags as json' do
+      tags = Tag.find_or_new('TA')
+      xhr :get, :tags, q: 'TA', format: :js
+      expect(response.body).to eq(tags.to_json)
+    end
+    
   end
 
 end
