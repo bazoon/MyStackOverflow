63371552b921a85debd419ecbdda7d9a51eea737
bazoon <vith77@gmail.com>
2014-10-29 12:27:03 +0500

    commented ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,57 +1,57 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
+  # def initialize(user)
 
-    if user
+  #   if user
 
-      can :ask, Question
+  #     can :ask, Question
 
-      can :write, Comment
+  #     can :write, Comment
       
 
-      can :manage, Comment do |comment|
-        comment.user == user
-      end
+  #     can :manage, Comment do |comment|
+  #       comment.user == user
+  #     end
 
-      can :destroy, Question do |question|
-        question.user == user
-      end
+  #     can :destroy, Question do |question|
+  #       question.user == user
+  #     end
 
-      can :edit, Question do |question|
-        question.user == user
-      end
+  #     can :edit, Question do |question|
+  #       question.user == user
+  #     end
 
-      can :update, Question do |question|
-        question.user == user
-      end
+  #     can :update, Question do |question|
+  #       question.user == user
+  #     end
 
-      can :rate, Question do |question|
-        question.user != user && !Vote.voted?(question, user)
-      end
+  #     can :rate, Question do |question|
+  #       question.user != user && !Vote.voted?(question, user)
+  #     end
 
-      can :rate, Answer do |answer|
-        answer.user != user && !Vote.voted?(answer, user)
-      end
+  #     can :rate, Answer do |answer|
+  #       answer.user != user && !Vote.voted?(answer, user)
+  #     end
       
-      can :destroy, Answer do |answer|
-        answer.user == user
-      end
+  #     can :destroy, Answer do |answer|
+  #       answer.user == user
+  #     end
 
-      can :update, Answer do |answer|
-        answer.user == user
-      end
+  #     can :update, Answer do |answer|
+  #       answer.user == user
+  #     end
 
-      can :select, Answer do |answer|
-        answer.question.user == user
-      end
+  #     can :select, Answer do |answer|
+  #       answer.question.user == user
+  #     end
 
 
       
 
 
 
-    end
+  #   end
 
-  end
+  # end
 end
