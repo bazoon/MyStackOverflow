5f2a8e9c7ad0bacc9eed6ac30f0c3193494c9c1f
bazoon <vith77@gmail.com>
2014-11-18 19:06:44 +0500

    sphinx part

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+
+  before_action :load_type
+
+  def index
+    @found_objects = @search_object.search(params[:search])
+  end
+
+  private
+
+  def load_type
+     @search_object = params[:type].constantize if %w[ThinkingSphinx Question Answer Comment].include?(params[:type])
+  end
+
+end
