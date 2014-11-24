57497669e7c1f052d9c17e48013e4e9cdd7f65e4
bazoon <vith77@gmail.com>
2014-11-24 10:39:52 +0500

    cap configuration added

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# config valid only for Capistrano 3.1
+lock '3.2.1'
+
+set :application, 'MyStackOverflow'
+set :repo_url, 'https://github.com/bazoon/MyStackOverflow.git'
+
+
+# Default deploy_to directory is /var/www/my_app
+set :deploy_to, '/home/deployer/so'
+set :deploy_user, 'deployer'
+
+# Default value for :scm is :git
+set :scm, :git
+
+
+# Default value for :linked_files is []
+set :linked_files, %w{config/database.yml config/private_pub.yml}
+
+# Default value for linked_dirs is []
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+ 
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,36 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+role :app, %w{deployer@185.31.161.45}
+role :web, %w{deployer@185.31.161.45}
+role :db,  %w{deployer@185.31.161.45}
+
+set :rails_env, :production 
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+server '185.31.161.45', user: 'deployer', roles: %w{web app db}, primary: true 
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+# TODO: ASK about rsa keys, github
+ set :ssh_options, {
+   keys: %w(/Users/vitaliynesterenko/.ssh/id_rsa),
+   forward_agent: true,
+   auth_methods: %w(publickey password), 
+   port: 2222
+ }
+
